8beb44a7564d432b9278e436c4fe75fdc559b633
SajanVirani09 <119833773+sajanvirani@users.noreply.github.com>
2025-04-01 23:03:52 +0530

    Fixed Some UI changes

--- src/components/JourneySection.tsx
+++ src/components/JourneySection.tsx
@@ -3,92 +3,89 @@ import { StatsData } from "../utils/consents.ts";
 
 const JourneySection = () => {
   return (
- 
+    <section className="container mx-auto px-12 pt-0 2xl:pt-16 relative flex flex-col md:flex-row justify-between items-center gap-6 md:gap-12 lg:gap-16 2xl:mt-44">
+      <div className="max-w-xl md:max-w-3xl lg:max-w-5xl">
+        <h2 className="text-3xl sm:text-4xl text-center md:text-start md:text-5xl lg:text-6xl font-medium mb-8">
+          Our Journey Towards{" "}
+          <span className="text-[#9e238d]">Excellence!</span>
+        </h2>
+        <p className="text-black mt-4 text-lg md:text-2xl">
+          Immensely grateful for the journey we&apos;ve undertaken together,
+          reaching this far with your unwavering support. Our story
+          wouldn&apos;t be complete without you. We consider ourselves blessed
+          for the opportunity to deliver your dream home and be a part of your
+          journey.
+        </p>
 
+        <div className="flex  flex-wrap gap-x-20 md:gap-x-0  md:flex-row justify-center md:justify-start gap-4 md:gap-16  lg:gap-24 mt-12 text-[#9e238d] font-medium">
+          {StatsData.map((stat, index) => (
+            <div
+              className="flex flex-col items-center space-y-5  md:space-y-10"
+              key={index}
+            >
+              <span
+                className={`text-4xl  text-[#9e238d] text-center font-medium  md:text-2xl lg:text-[55px]`}
+              >
+                {stat.value}
+              </span>
+              <p className="text-black text-center text-base md:text-lg lg:text-4xl font-medium capitalize">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
 
+      {/* House Shape Image */}
+      <div className="relative">
+        <div className="w-56 h-64 md:w-60 lg:w-80 mx-auto md:h-[350px] lg:h-[450px] bg-cover bg-center">
+          <img
+            src="/assets/images/gallery/home.gif"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+    </section>
 
-    <section className="container mx-auto px-12 pt-0 2xl:pt-16    relative flex flex-col md:flex-row justify-between items-center gap-6 md:gap-12 lg:gap-16">
-  <div className="max-w-xl md:max-w-3xl lg:max-w-5xl">
-    <h2 className="text-3xl sm:text-4xl text-center md:text-start md:text-5xl lg:text-6xl font-medium mb-8">
-      Our Journey Towards{" "}
-      <span className="text-[#9e238d]">Excellence!</span>
-    </h2>
-    <p className="text-black mt-4 text-lg md:text-2xl">
-      Immensely grateful for the journey we&apos;ve undertaken together,
-      reaching this far with your unwavering support. Our story
-      wouldn&apos;t be complete without you. We consider ourselves blessed
-      for the opportunity to deliver your dream home and be a part of your
-      journey.
-    </p>
+    //     <section className=" max-w-7xl lg:container mt-16 md:mt-24 lg:mt-28 px-6 md:px-8 lg:px-10 relative flex flex-col md:flex-row justify-between items-center">
+    //   <div className="max-w-xl md:max-w-3xl lg:max-w-5xl">
+    //     <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl  lg:text-5xl   tracking-wide  mb-4">
+    //       Our Journey Towards <span className="text-[#9e238d]">Excellence!</span>
+    //     </h2>
+    //     <p className="text-black mt-4 text-base md:text-lg lg:text-xl">
+    //       Immensely grateful for the journey we've undertaken together,
+    //       reaching this far with your unwavering support. Our story
+    //       wouldn’t be complete without you. We consider ourselves blessed
+    //       for the opportunity to deliver your dream home and be a part of your
+    //       journey.
+    //     </p>
 
-    <div className="flex  flex-wrap gap-x-20 md:gap-x-0  md:flex-row justify-center md:justify-start gap-4 md:gap-16  lg:gap-24 mt-12 text-[#9e238d] font-medium">
-  {StatsData.map((stat, index) => (
-    <div className="flex flex-col items-center space-y-5  md:space-y-10" key={index}>
-      <span
-        className={`text-4xl  text-[#9e238d] text-center font-medium  md:text-2xl lg:text-[55px]`}
-      >
-        {stat.value}
-      </span>
-      <p className="text-black text-center text-base md:text-lg lg:text-4xl font-medium capitalize">
-        {stat.label}
-      </p>
-    </div>
-  ))}
-</div>
-  </div>
+    //     {/* Stats Section */}
+    //     <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8 text-[#9e238d] font-medium">
+    //       {StatsData.map((stat, index) => (
+    //         <div key={index} className="flex flex-col items-center">
+    //           <span className={`text-[#9e238d] font-medium text-2xl sm:text-3xl md:text-4xl lg:text-5xl  lg:text-5xl `}>
+    //             {stat.value}
+    //           </span>
+    //           <p className="text-black text-base md:text-lg lg:text-xl capitalize">
+    //             {stat.label}
+    //           </p>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
 
-  {/* House Shape Image */}
-  <div className="relative">
-    <div className="w-56 h-64 md:w-60 lg:w-80 mx-auto md:h-[350px] lg:h-[450px] bg-cover bg-center">
-      <img
-        src="/assets/images/gallery/home.gif"
-        className="h-full w-full object-cover"
-      />
-    </div>
-  </div>
-</section>
-
-
-//     <section className=" max-w-7xl lg:container mt-16 md:mt-24 lg:mt-28 px-6 md:px-8 lg:px-10 relative flex flex-col md:flex-row justify-between items-center">
-//   <div className="max-w-xl md:max-w-3xl lg:max-w-5xl">
-//     <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl  lg:text-5xl   tracking-wide  mb-4">
-//       Our Journey Towards <span className="text-[#9e238d]">Excellence!</span>
-//     </h2>
-//     <p className="text-black mt-4 text-base md:text-lg lg:text-xl">
-//       Immensely grateful for the journey we've undertaken together,
-//       reaching this far with your unwavering support. Our story
-//       wouldn’t be complete without you. We consider ourselves blessed
-//       for the opportunity to deliver your dream home and be a part of your
-//       journey.
-//     </p>
-
-//     {/* Stats Section */}
-//     <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8 text-[#9e238d] font-medium">
-//       {StatsData.map((stat, index) => (
-//         <div key={index} className="flex flex-col items-center">
-//           <span className={`text-[#9e238d] font-medium text-2xl sm:text-3xl md:text-4xl lg:text-5xl  lg:text-5xl `}>
-//             {stat.value}
-//           </span>
-//           <p className="text-black text-base md:text-lg lg:text-xl capitalize">
-//             {stat.label}
-//           </p>
-//         </div>
-//       ))}
-//     </div>
-//   </div>
-
-//   {/* House Shape Image */}
-//   <div className="relative hidden md:block">
-//     <div className="w-48 h-48 md:w-60 lg:w-80 mx-auto md:h-[350px] lg:h-[450px] bg-cover bg-center">
-//       <img
-//         src="/assets/images/gallery/home.gif"
-//         className="h-full w-full object-cover"
-//         alt="Home Journey"
-//       />
-//     </div>
-//   </div>
-// </section>
-
+    //   {/* House Shape Image */}
+    //   <div className="relative hidden md:block">
+    //     <div className="w-48 h-48 md:w-60 lg:w-80 mx-auto md:h-[350px] lg:h-[450px] bg-cover bg-center">
+    //       <img
+    //         src="/assets/images/gallery/home.gif"
+    //         className="h-full w-full object-cover"
+    //         alt="Home Journey"
+    //       />
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
 
--- src/components/TestimonialSlider.tsx
+++ src/components/TestimonialSlider.tsx
@@ -6,119 +6,112 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Testimonials } from "../utils/consents.ts";
 
-
 const TestimonialSlider = () => {
   return (
-
     <section className="px-4 md:px-10  relative overflow-x-hidden  ">
-    <div className="mx-auto">
-    <h2 className="text-2xl sm:text-4xl md:text-3xl lg:text-5xl font-medium text-gray-900 leading-[1.4] md:leading-[1.6] lg:leading-[74px]">
-  Hear from our <span className="block text-[#9f238a]">Customers</span>
-</h2>
-    </div>
-  
-    <div className="mt-6 mx-auto my-12 ">
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={20}
-        slidesPerView={1}
-        loop={true}
-        autoplay={{ delay: 3000 }}
-        pagination={{ 
-          clickable: true,
-          el: '.custom-pagination', 
-        }}
-        breakpoints={{
-          640: { slidesPerView: 1 },
-          768: { slidesPerView: 2 },
-          1140: { slidesPerView: 3 },
-        }}
-        className="p-4"
-      >
-        {Testimonials.map((item, index) => (
-          <SwiperSlide key={index}>
-            <div className={`p-5 border-2 sm:h-[290px]  md:h-[310px] lg:w-[420px]  2xl:w-[460px] lg:h-[330px] rounded-md ${index % 2 === 1 ? "bg-[#C69E58]/50 border-[#C69E58]" : "bg-[#FEE9FE7D]/50 border-[#9F238B] "} relative`}>
-              <h3 className="text-lg ">{item.name}</h3>
-              <p className="text-base">{item.role}</p>
-              <p className="mt-8 text-lg ">{item.review}</p>
-              <p className="mt-2 text-sm text-gray-500">{item.date}</p>
-              {item.rating && (
-                <div className="absolute top-3 right-4">
-                  {"★".repeat(item.rating)}
-                  {"☆".repeat(5 - item.rating)}
-                  <p className="mt-2 text-sm">{item.date}</p>
-                </div>
-              )}
-            </div>
-          </SwiperSlide>
-       
-        ))}
-      </Swiper>
-  
-  
-      <div className="pr-10  custom-pagination absolute right-4 bottom-4 flex flex-col items-end gap-2"></div>
-    </div>
-  </section>
+      <div className="mx-auto">
+        <h2 className="text-2xl sm:text-4xl md:text-3xl lg:text-5xl font-medium text-gray-900 leading-[1.4] md:leading-[1.6] lg:leading-[74px]">
+          Hear from our <span className="block text-[#9f238a]">Customers</span>
+        </h2>
+      </div>
 
-//     <section className="px-4 md:px-8 lg:px-12 relative overflow-x-hidden">
-//   <div className="container mx-auto">
+      <div className="mt-6 mx-auto my-12 ">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={20}
+          slidesPerView={1}
+          // loop={true}
+          // autoplay={{ delay: 3000 }}
+          pagination={{
+            clickable: true,
+            el: ".custom-pagination",
+          }}
+          breakpoints={{
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1140: { slidesPerView: 3 },
+          }}
+          className="p-4"
+        >
+          {Testimonials.map((item, index) => (
+            <SwiperSlide key={index}>
+              <div
+                className={`p-5 border-2 sm:h-[290px]  md:h-[310px] lg:h-[330px] rounded-md ${
+                  index % 2 === 1
+                    ? "bg-[#C69E58]/50 border-[#C69E58]"
+                    : "bg-[#FEE9FE7D]/50 border-[#9F238B] "
+                } relative`}
+              >
+                <h3 className="text-lg xl:text-xl">{item.name}</h3>
+                <p className="text-base">{item.role}</p>
+                <p className="mt-8 text-lg xl:text-xl pe-16">{item.review}</p>
+                <p className="mt-2 text-sm text-gray-500 absolute bottom-4">{item.date}</p>
+                {item.rating && (
+                  <div className="absolute top-3 right-4">
+                    {"★".repeat(item.rating)}
+                    {"☆".repeat(5 - item.rating)}
+                    <p className="mt-2 text-sm">{item.date}</p>
+                  </div>
+                )}
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
 
-//     <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl   font-medium  tracking-wide text-gray-900 leading-loose md:leading-snug ">
-//       Hear from our <br /> 
-//       <span className="text-[#905c87]">Customers</span>
-//     </h2>
+        <div className="pr-10  custom-pagination absolute right-4 bottom-4 flex flex-col items-end gap-2"></div>
+      </div>
+    </section>
 
+    //     <section className="px-4 md:px-8 lg:px-12 relative overflow-x-hidden">
+    //   <div className="container mx-auto">
 
-//     <div className="mt-6 mx-auto my-12">
-//       <Swiper
-//         modules={[Navigation, Pagination, Autoplay]}
-//         spaceBetween={20}
-//         slidesPerView={1}
-//         loop={true}
-//         autoplay={{ delay: 3000 }}
-//         pagination={{ 
-//           clickable: true,
-//           el: '.custom-pagination', 
-//         }}
-//         breakpoints={{
-//           640: { slidesPerView: 1 },
-//           768: { slidesPerView: 2 },
-//           1024: { slidesPerView: 3 },
-//         }}
-//         className="p-4"
-//       >
-//         {Testimonials.map((item, index) => (
-//           <SwiperSlide key={index}>
-//             <div className={`p-5 border-2 sm:h-[290px] lg:w-[460px] lg:h-[330px] rounded-md ${index % 2 === 1 ? "bg-[#f5efe1] border-[#905c87]" : "bg-pink-100 border-[#905c87]"} relative`}>
-//               <h3 className="text-lg sm:text-xl md:text-2xl">{item.name}</h3>
-//               <p className="text-base text-black/60">{item.role}</p>
-//               <p className="mt-6 text-sm sm:text-base lg:text-lg">{item.review}</p>
-//               <p className="mt-2 text-xs sm:text-base text-black/60">{item.date}</p>
-//               {item.rating && (
-//                 <div className="absolute top-3 right-4">
-//                   {"★".repeat(item.rating)}
-//                   {"☆".repeat(5 - item.rating)}
-//                   <p className="mt-2 text-xs sm:text-sm">{item.date}</p>
-//                 </div>
-//               )}
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
+    //     <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl   font-medium  tracking-wide text-gray-900 leading-loose md:leading-snug ">
+    //       Hear from our <br />
+    //       <span className="text-[#905c87]">Customers</span>
+    //     </h2>
 
-  
-//       <div className="pr-10 custom-pagination absolute right-4 bottom-4 flex flex-col items-end gap-2"></div>
-//     </div>
-//   </div>
-// </section>
+    //     <div className="mt-6 mx-auto my-12">
+    //       <Swiper
+    //         modules={[Navigation, Pagination, Autoplay]}
+    //         spaceBetween={20}
+    //         slidesPerView={1}
+    //         loop={true}
+    //         autoplay={{ delay: 3000 }}
+    //         pagination={{
+    //           clickable: true,
+    //           el: '.custom-pagination',
+    //         }}
+    //         breakpoints={{
+    //           640: { slidesPerView: 1 },
+    //           768: { slidesPerView: 2 },
+    //           1024: { slidesPerView: 3 },
+    //         }}
+    //         className="p-4"
+    //       >
+    //         {Testimonials.map((item, index) => (
+    //           <SwiperSlide key={index}>
+    //             <div className={`p-5 border-2 sm:h-[290px] lg:w-[460px] lg:h-[330px] rounded-md ${index % 2 === 1 ? "bg-[#f5efe1] border-[#905c87]" : "bg-pink-100 border-[#905c87]"} relative`}>
+    //               <h3 className="text-lg sm:text-xl md:text-2xl">{item.name}</h3>
+    //               <p className="text-base text-black/60">{item.role}</p>
+    //               <p className="mt-6 text-sm sm:text-base lg:text-lg">{item.review}</p>
+    //               <p className="mt-2 text-xs sm:text-base text-black/60">{item.date}</p>
+    //               {item.rating && (
+    //                 <div className="absolute top-3 right-4">
+    //                   {"★".repeat(item.rating)}
+    //                   {"☆".repeat(5 - item.rating)}
+    //                   <p className="mt-2 text-xs sm:text-sm">{item.date}</p>
+    //                 </div>
+    //               )}
+    //             </div>
+    //           </SwiperSlide>
+    //         ))}
+    //       </Swiper>
 
+    //       <div className="pr-10 custom-pagination absolute right-4 bottom-4 flex flex-col items-end gap-2"></div>
+    //     </div>
+    //   </div>
+    // </section>
+  );
+};
 
-
-
-
-
-  )
-  
-}
-
-export default TestimonialSlider
+export default TestimonialSlider;
